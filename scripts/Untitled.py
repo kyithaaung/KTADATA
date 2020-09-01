e1d5a1a1697b9a212367eceb0a4c8aac02a3b486
KTA <kyitha91@gmail.com>
2020-09-02 00:26:49 +0630

    test script

---
+++ scripts/Untitled.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+
+print("done")
+
+
+# In[ ]:
+
+
+
+
